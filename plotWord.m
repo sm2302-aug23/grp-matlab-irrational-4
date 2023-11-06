292aca9646f5a775c0f4e7cd27142d93b6e24882
Azyan <141397076+azyansafriudin@users.noreply.github.com>
2023-11-06 12:22:19 +0800

    Update plotWord.m

--- plotWord.m
+++ plotWord.m
@@ -5,7 +5,7 @@ function plotWord(word)
     % Convert the input word to lowercase
     word = lower(word);
 
-    % Initialize variables for plotting coordinates
+    % Initialise variables for plotting coordinates
     wordCoordinates = [];
 
     % Shift value for x-coordinate
